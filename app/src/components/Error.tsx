e81db4083b6d32266ee3d45cf71834e206bb4e3e
Rami Karhi <rami.karhi@gmail.com>
2022-07-06 19:38:01 +0300

    Center align

--- app/src/components/Error.tsx
+++ app/src/components/Error.tsx
@@ -8,7 +8,7 @@ import sad from './sad.svg'
 function Loading() {
   return (
     <Fade in={true}>
-      <Box sx={{ display: 'flex', flexDirection: 'column', justifyContent: 'center', mt: 2 }}>
+      <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'center', mt: 2 }}>
         <img src={sad} height={100} width={100} alt="sad" />
         <Typography variant='h3' sx={{ mt: -3 }}>Oops, something went wrong!</Typography>
         <Typography variant='h3' color="secondary">Please try again later.</Typography>
